5a34d69eb2d0d62b2c3a4cc4607dbf5eb521d2f5
Zach Power <zachpwr@users.noreply.github.com>
2016-05-09 11:13:21 -0400

    Consolidated code

--- Makefile
+++ Makefile
@@ -1,33 +1,33 @@
-CCARGS = -mmcu=atmega103 -DF_CPU=16000000 -Os
+CCARGS = -g -Wall -mmcu=atmega328 -01
 
-default: test
+default: main
 
-test: test.c motor.o switch.o
-	avr-gcc  $(CCARGS) switch.o motor.o test.c -o test 
+main: main.c motor.o sensor.o qtr_driver.o
+	avr-gcc  $(CCARGS) switch.o motor.o test.c -o test
 
-motor.o: motor.c motor.h io_softcore.h
-	avr-gcc  $(CCARGS) -c motor.c motor.h io_softcore.h 
+motor.o: motor.c motor.h
+	avr-gcc  $(CCARGS) -c motor.c motor.h
 
-switch.o: switch.c switch.h io_softcore.h
-	avr-gcc $(CCARGS) -c switch.c switch.h io_softcore.h
+sensor.o: sensor.c sensor.h qtr_driver.h
+	avr-gcc $(CCARGS) -c switch.c switch.h qtr_driver.h
 
-test.hex: test
-	avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock test test.hex
+main.hex: main
+	avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock main main.hex
 
-test.mem: test.hex
-	srec_cat test.hex -Intel -Byte_Swap 2 -Data_Only \
-	-Output_Block_Size 32 -o test.mem -vmem 8
+main.mem: main.hex
+	srec_cat main.hex -Intel -Byte_Swap 2 -Data_Only \
+	-Output_Block_Size 32 -o main.mem -vmem 8
 
-test.bit: test.mem
+main.bit: main.mem
 	/opt/Xilinx/14.6/ISE_DS/ISE/bin/lin64/data2mem       \
 	-bm Papilio_AVR8_bd.bmm -bt Papilio_AVR8.bit \
-	-bd test.mem -o b test.bit
+	-bd main.mem -o b main.bit
 
-fpga: test.bit
-	papilio-prog -f test.bit
+fpga: main.bit
+	papilio-prog -f main.bit
 
 prom: test.bit
-	papilio-prog -b /usr/share/papilio/bscan_spi_xc3s500e.bit -f test.bit
+	papilio-prog -b /usr/share/papilio/bscan_spi_xc3s500e.bit -f main.bit
 
 clean:
-	-rm test.bit test.mem test.hex test *.o	 *.gch
+	-rm main.bit main.mem main.hex main *.o	 *.gch
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include "main.h"
 #include "rover.h"
 #include "sensor.h"
 #include "motor.h"
@@ -6,7 +7,9 @@
 
 int main(){
     init_QTR_driver();
-    solveMaze();
+    unsigned char firstState = getSensorData();
+    solveMaze(firstState);
+    return 0;
 }
 
 void solveMaze(unsigned char previousState) {
@@ -27,4 +30,137 @@ void solveMaze(unsigned char previousState) {
     }
 
     solveMaze(currentState);
+}
+
+unsigned char getSensorData() {
+    return get_QTR_value();
+}
+
+int isLeftTurnAvailable(unsigned char sensorData) {
+    return ((sensorData & 0xC0) >> 6) == 3;
+}
+
+int isRightTurnAvailable(unsigned char sensorData) {
+    return (sensorData & 0x03) == 3;
+}
+
+int isOnPath(unsigned char sensorData) {
+    return ((sensorData & 0x3C) >> 2) == 0x0F;
+}
+
+int isTooFarLeft(unsigned char sensorData) {
+    return ((sensorData & 0x0F) << 4 | (sensorData & 0xF0) >> 4) > sensorData;
+}
+
+int isTooFarRight(unsigned char sensorData) {
+    return ((sensorData & 0x0F) << 4 | (sensorData & 0xF0) >> 4) < sensorData;
+}
+
+int isOffOfPath(unsigned char sensorData) {
+    return sensorData == 0x00;
+}
+
+// -- HIGH-LEVEL MOTOR I/O -- //
+void drift(int direction) {
+    int leftDirection = (direction < 0) ? BREAK : FORWARD; // Dictates left/right turn depending on sign of input value
+    int rightDirection = (direction < 0) ? FORWARD : BREAK;
+
+    setLeftMotor(leftDirection);
+    setRightMotor(rightDirection);
+
+    _delay_ms(50);
+
+    stop();
+    return;
+}
+
+void turn(int direction) {
+    int leftDirection = (direction > 0) ? FORWARD : BACKWARD; // Dictates left/right turn depending on sign of input value
+    int rightDirection = (direction > 0) ? BACKWARD : FORWARD;
+
+    while(!isOnPath()) { // While loop that continues until rover has completed 90degree turn
+        setRightMotor(rightDirection);
+        setLeftMotor(leftDirection);
+        _delay_ms(10);
+    }
+
+    stop();
+    return;
+}
+
+void driveForward() {
+    setRightMotor(FORWARD);
+    setLeftMotor(FORWARD);
+    _delay_ms(10);
+
+    stop();
+    return;
+}
+
+int stop() {//Makes rover stop moving
+    setLeftMotor(BREAK);
+    setRightMotor(BREAK);
+}
+
+// -- LOW-LEVEL MOTOR I/O -- //
+int setLeftMotor(int direction) { // Gives access to the left motor
+    static int currentDirection;
+    if(currentDirection == direction) return 0;
+
+    LEFT_PORT_OUT &= ~LEFT_MOTOR_MASK;
+    LEFT_PORT_OUT |= direction << 6;
+
+    currentDirection = direction;
+    return 1;
+}
+
+int setRightMotor(int direction) { // Gives access to the right motor
+    static int currentDirection;
+    if(currentDirection == direction) return 0;
+
+    LEFT_PORT_OUT &= ~RIGHT_MOTOR_MASK;
+    LEFT_PORT_OUT |= direction << 2;
+
+    currentDirection = direction;
+    return 1;
+}
+
+unsigned char QTR_ARRAY_VALUE; //variable holding current QTR sensor state
+
+//returns the current state of the QTR array
+unsigned char get_QTR_value()
+{
+	return QTR_ARRAY_VALUE;
+}
+
+//initializes the QTR interrupt-based driver
+void init_QTR_driver()
+{
+	QTR_ARRAY_VALUE=0x00; //initial value
+	DDR_QTR=0x00;   //configure port as input...
+	PORT_QTR=0x00;  //in order to deactivate pull-up resistors
+	OCR0B = QTR_CHARGE_TIME; //time we turn off the array charge
+	OCR0A = QTR_DISCHARGE_TIME; //time we read the array
+	TCCR0A |= (1<<WGM01); //configure counter to reset at QTR_DISCHARGE_TIME
+	TIMSK0 |= (1 << OCIE0A) | (1 << OCIE0B);  //enable interrupts
+	TCCR0B |= ((1<<CS02) | (1<<CS00)); //set counter prescaler to 1024 and start counter
+	sei(); //enable global interrupts
+	return;
+}
+void QTR_CHARGE_ARRAY()
+{
+	PORT_QTR=0xFF; //set array ping to output, then
+	DDR_QTR=0xFF; //output 0xFF to QTR pins to charge array
+	return;
+}
+ISR(TIMER0_COMPB_vect)
+{
+	DDR_QTR = 0x00;  //configure QTR port as input to start discharging
+	return;
+}
+ISR(TIMER0_COMPA_vect)
+{
+	QTR_ARRAY_VALUE=PIN_QTR; //read current sensor value
+	QTR_CHARGE_ARRAY(); //begin chrging array for next read
+	return;
 }
